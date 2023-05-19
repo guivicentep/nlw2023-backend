d10ceae093e3f00151ca9b74375835fd958801d0
guivicentep <guivicente.gvp@hotmail.com>
2023-05-19 13:58:59 -0300

    feat: Criação do CRUD das memories

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, // todas as urls de front-end poderão acessar nosso back-end
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
